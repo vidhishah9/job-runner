6ce775262a0a91cc8ee90656292817112cb52993
Vidhi Shah <happyshah909@gmail.com>
2026-01-20 00:51:24 -0800

    created worker

--- main.go
+++ main.go
@@ -4,7 +4,17 @@ import (
     "bufio"
     "fmt"
     "os"
+    "time"
 )
+
+func worker(id int, tasks <-chan string) {
+    fmt.Println("Starting worker", id)
+
+    for task := range tasks {
+        fmt.Println(task)
+    }
+}
+
 
 func main() {
 
@@ -23,17 +33,23 @@ func main() {
 	tasks := make(chan string)
 	//start a goroutine to read the file line by line and send each line to the tasks channel
     go readFileLineByLine(tasks, scanner)
-    task := <-tasks
-    fmt.Println(task)
 
+    time.Sleep(10 * time.Millisecond) //wait a bit for queue to load some tasks
+
+    //start 3 worker goroutines to receive tasks from the tasks channel and print them
+    for w := 1; w <= 3; w++ {
+        go worker(w, tasks)
+    }
+    time.Sleep(50 * time.Millisecond) //make sure main doesn't exit before goroutines finish
 }
+
 
 func readFileLineByLine(tasks chan <-string, scanner *bufio.Scanner) {
     fmt.Println("Reading file line by line...")
 	// Read and print lines
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Println(line)
+        // fmt.Println(line)
 
         tasks <- line
     }
